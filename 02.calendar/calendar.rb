d012ac921692555dcc86ed26c765ab65a81cc796
o-musubi <okita.pub.0204@gmail.com>
2023-03-13 02:38:45 +0900

    インスタンス変数を使わないように構造を変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,54 +1,35 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'date'
 require 'optparse'
 
-# 今日の月、年を変数に代入
-def today
-  @month = Date.today.month
-  @year = Date.today.year
+def option_valid?
+  params = ARGV.getopts('m:', 'y:')
+  option = { month: params['m'].to_i, year: params['y'].to_i }
+  option[:month] = Date.today.month unless (1..12).cover?(option[:month])
+  option[:year] = Date.today.year unless (1970..2100).cover?(option[:year])
+  option
 end
 
-# オプションの引数として指定した数字があれば変数に代入
-def to_i_arguments
-  params = ARGV.getopts("m:y:")
-  @month = params["m"].to_i  unless params["m"] == nil
-  @year = params["y"].to_i unless params["y"] == nil
+def first_last
+  base_date = option_valid?
+  target_period = {}
+  target_period[:first] = Date.new(base_date[:year], base_date[:month], 1)
+  target_period[:last] = Date.new(base_date[:year], base_date[:month], -1)
+  target_period
 end
 
-# 想定していない引数が指定された場合今日の月、年を変数に代入
-def unexpected_arguments
-  unless (1..12).cover?(@month) && (1970..2100).cover?(@year)
-    puts "想定していない引数が与えられました。\nデフォルト値(今日)を指定します。" ,"\s"
-    today
+def print_calendar
+  print_period = first_last
+  puts "#{'\s' * 5}#{print_period[:first].month}月\\s#{print_period[:first].year}"
+  %(日 月 火 水 木 金 土).each { |str| print str, '\s' }
+  print '\n'
+  print_period[:first].wday.times { print '\s' * 3 }
+  (print_period[:first]..print_period[:last]).each do |date|
+    print date.strftime('%e'), '\s'
+    print '\n' if date.saturday?
   end
 end
 
-# (日付を繰り返し表示するための)基準日
-def base_date 
-  @first_date = Date.new(@year,@month,1)
-  @last_date = Date.new(@year,@month,-1)
-end
-
-# カレンダーのヘッダー部分を出力する
-def output_header
-  puts "\s" * 5 + "#{@month}月" + "\s" + "#{@year}"
-  ["日", "月", "火", "水", "木", "金", "土"].each {|str| print str, "\s"}
-  print "\n"
-  @first_date.wday.times {print "\s" * 3}
-end
-
-# カレンダーの日付の部分を出力する
-def output_main
-  (@first_date..@last_date).each do |a_date| 
-  print  a_date.strftime("%e"), "\s"
-  print "\n" if a_date.saturday?
-  end
-end
-
-today
-to_i_arguments
-unexpected_arguments
-base_date
-output_header
-output_main
+print_calendar
