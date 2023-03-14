5ea5cb3015a3badabf6e68a1ea8075d6b549fb25
o-musubi <okita.pub.0204@gmail.com>
2023-03-15 03:32:39 +0900

    変数名、半角スペースの記述方法を変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -6,10 +6,10 @@ require 'optparse'
 
 def recieve_options
   params = ARGV.getopts('m:', 'y:')
-  recieved_options = { month: params['m'].to_i, year: params['y'].to_i }
-  recieved_options[:month] = Date.today.month unless (1..12).cover?(recieved_options[:month])
-  recieved_options[:year] = Date.today.year unless (1970..2100).cover?(recieved_options[:year])
-  recieved_options
+  options = { month: params['m'].to_i, year: params['y'].to_i }
+  options[:month] = Date.today.month unless (1..12).cover?(options[:month])
+  options[:year] = Date.today.year unless (1970..2100).cover?(options[:year])
+  options
 end
 
 def determine_dates
@@ -22,9 +22,9 @@ end
 
 def print_calendar
   dates = determine_dates
-  puts "#{"\s" * 5}#{dates[:first].month}月 #{dates[:first].year}"
+  puts "     #{dates[:first].month}月 #{dates[:first].year}"
   puts '日 月 火 水 木 金 土'
-  dates[:first].wday.times { print "\s" * 3 }
+  dates[:first].wday.times { print '   ' }
   (dates[:first]..dates[:last]).each do |date|
     print date.strftime('%e'), "\s"
     print "\n" if date.saturday?
