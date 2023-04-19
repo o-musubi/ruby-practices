0da3b85ad20d72ab956f4cfc8661ab367bf64460
o-musubi <okita.pub.0204@gmail.com>
2023-04-20 02:07:18 +0900

    lsコマンド1を作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,22 +1,37 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-file_lists = Dir.glob('*')
-maximum_columns = 3
-column_difference = file_lists.size.ceildiv(maximum_columns)
-displayed_columns = file_lists.sort.each_slice(column_difference).to_a
-
-displayed_columns.each.with_index(1).cycle(column_difference) do |column, index|
-  answer = [] << column.shift
-  if column_difference < maximum_columns && !answer[0].nil? && index == (maximum_columns - 1)
-    printf('%-30s', answer[0])
-    puts "\n"
-  elsif !answer[0].nil? && index == maximum_columns
-    printf('%-30s', answer[0])
-    puts "\n"
-  elsif !answer[0].nil?
-    printf('%-30s', answer[0])
-  else
-    print ''
-    puts "\n"
+def main
+  file = format_file
+  file[:splited_columns].each.with_index(1).cycle(file[:column_difference]) do |column, index|
+    displayed_columns = [] << column.shift
+    if displayed_columns[0].nil?
+      puts ' '
+    elsif file[:splited_columns[file[:specific_number]]].nil? && index == file[:specific_number]
+      printf('%-20s', displayed_columns[0])
+      puts ' '
+    elsif index == file[:maximum_columns]
+      printf('%-20s', displayed_columns[0])
+      puts ' '
+    else
+      printf('%-20s', displayed_columns[0])
+    end
   end
 end
+
+def set_conditions
+  {
+    file: Dir.glob('*'),
+    maximum_columns: 3
+  }
+end
+
+def format_file
+  condition = set_conditions
+  condition[:column_difference] = condition[:file].size.ceildiv(condition[:maximum_columns])
+  condition[:splited_columns] = condition[:file].sort.each_slice(condition[:column_difference]).to_a
+  condition[:specific_number] = condition[:splited_columns].size
+  condition
+end
+
+main
