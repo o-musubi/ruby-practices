615ed672f556922009c166c1216105c9ee107475
o-musubi <okita.pub.0204@gmail.com>
2023-05-10 00:02:07 +0900

    軽微な修正(定数の位置、メソッド名、不要なメソッドと変数の削除)

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+MAXIMUM_COLUMNS = 3
+
 def main
   file_names = find_file_names
   formatted_file_names = format_file_names(file_names)
@@ -13,26 +15,18 @@ end
 
 def format_file_names(found_file_names)
   file_names = found_file_names
-  difference = column_difference(file_names)
-  columns = splited_columns(file_names, difference)
-  file_names_to_display = swap_file_names(difference, columns)
+  row_size = file_names.size.ceildiv(MAXIMUM_COLUMNS)
+  splitted_rows = file_names.each_slice(row_size).to_a
+  swap_file_names(row_size, splitted_rows)
 end
 
-def column_difference(file_names)
-  file_names.size.ceildiv(MAXIMUM_COLUMNS)
-end
-
-def splited_columns(file_names, difference)
-  file_names.each_slice(difference).to_a
-end
-
-def swap_file_names(difference, columns)
+def swap_file_names(row_size, rows)
   file_names_to_display = []
   chunk = []
-  specific_count = MAXIMUM_COLUMNS - columns.size
+  specific_count = MAXIMUM_COLUMNS - rows.size
 
-  columns.cycle(difference).with_index(1) do |column, i|
-    chunk << column.shift
+  rows.cycle(row_size).with_index(1) do |row, i|
+    chunk << row.shift
     if !specific_count.zero? && (i % (MAXIMUM_COLUMNS - specific_count)).zero? ||
        specific_count.zero? && (i % MAXIMUM_COLUMNS).zero?
       file_names_to_display << chunk.clone
@@ -50,5 +44,4 @@ def display_files(nested_file_names)
   end
 end
 
-MAXIMUM_COLUMNS = 3
 main
