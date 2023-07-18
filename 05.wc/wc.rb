77f2238a8132f2ff890ff6508cb3bb150a5f4190
o-musubi <okita.pub.0204@gmail.com>
2023-07-18 23:11:20 +0900

    指摘いただいた点を修正
    シンプルなメソッドに変更できる部分をすべて変更
    引数を暗黙的に破壊的に変更していた部分を修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -32,26 +32,23 @@ def select_source_data(file_names)
 end
 
 def open_file(file_names)
-  file_names.each_with_object([]) do |file_name, texts|
-    File.open(file_name) { |lines| texts << lines.readlines.join }
+  file_names.map do |file_name|
+    File.open(file_name) { |lines| lines.readlines.join }
   end
 end
 
 def count(texts)
   {
-    l: texts.each_with_object([]) { |text, lines_count| lines_count << text.count("\n") },
-    w: texts.each_with_object([]) { |text, words_count| words_count << text.squeeze(' ').split.size },
-    c: texts.each_with_object([]) { |text, byte_count| byte_count << text.bytesize }
+    l: texts.map { |text| text.count("\n") },
+    w: texts.map { |text| text.squeeze(' ').split.size },
+    c: texts.map(&:bytesize)
   }
 end
 
 def filter_data_by_argument(params, counted_data)
-  filtered_data = []
   params = { l: true, w: true, c: true } if params.empty?
-  %i[l w c].each do |param|
-    filtered_data << counted_data.fetch(param) if params[param]
-  end
-  filtered_data
+  filtered_data = counted_data.select { |key| params[key] }
+  filtered_data.values
 end
 
 def transform_data(filtered_data)
@@ -61,10 +58,10 @@ def transform_data(filtered_data)
       data: [filtered_data.flatten]
     }
   else
-    filtered_data.each { |array| array << array.sum }
+    added_total_data = filtered_data.dup.each { |data| data << data.sum }
     {
-      width: calculate_width(filtered_data),
-      data: sort_data(filtered_data)
+      width: calculate_width(added_total_data),
+      data: added_total_data.transpose
     }
   end
 end
@@ -73,28 +70,14 @@ def calculate_width(filtered_data)
   filtered_data.flatten.map { |data| data.to_s.bytesize }.max
 end
 
-def sort_data(filtered_data)
-  number_of_times = filtered_data[0].size
-  chunk = []
-  displayed_array = []
-  filtered_data.cycle(number_of_times) { |array| chunk << array.shift }
-  second_number_of_times = filtered_data.size
-  chunk.each_slice(second_number_of_times) { |data| displayed_array << data }
-  displayed_array
-end
-
 def output(displayed_data, file_names)
   if file_names.empty?
     displayed_data[:data].flatten.each { |data| print '     ', data.to_s }
     puts ' '
   else
-    file_names << 'total' unless displayed_data[:data].size == 1
     displayed_data[:data].each_with_index do |displayed_array, index|
-      displayed_array.each do |array|
-        print array.to_s.rjust(displayed_data[:width])
-        print ' '
-      end
-      puts file_names[index]
+      print displayed_array.map { |array| array.to_s.rjust(displayed_data[:width]) }.join(' ')
+      puts file_names[index] ? " #{file_names[index]}" : ' total'
     end
   end
 end
